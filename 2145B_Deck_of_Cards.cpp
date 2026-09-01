a468af43404407a92cb57880be7e03a09ce28474
nayani vansh <142765588+VANSHNAYANI@users.noreply.github.com>
2026-09-01 23:45:58 +0530

    Sync Codeforces: 2145B - Deck of Cards [C++17 (GCC 7-32)]

---
+++ 2145B_Deck_of_Cards.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define endl '\n'
+ 
+void solve()
+{
+    int n,k;
+    string s1;
+    cin >> n >> k >> s1;
+ 
+    int onecnt = count(s1.begin(),s1.end(),'1');
+    int zerocnt = count(s1.begin(),s1.end(),'0');
+    int twocnt = count(s1.begin(),s1.end(),'2');
+ 
+    vector<char>ans(n,'+');
+ 
+    for (int i = 0; i < n; i++)
+    {
+        if(i < zerocnt + twocnt  || i >= n - onecnt - twocnt)ans[i] = '?';
+        if(i < zerocnt ||i >= n - onecnt || k == n)ans[i] = '-';
+    }
+    
+    for(auto it : ans)
+    {
+        cout << it;
+    }
+    cout << "\n";
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
+ 
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+ 
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
